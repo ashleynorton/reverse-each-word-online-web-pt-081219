06db44b90b453401bb47406e3015c26bf6d10a09
Ashley Norton <nortonashleym@gmail.com>
2019-08-25 22:58:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.each do |word|
-    reversed_words = word.reverse
-    array.push(reversed_words)
+  array.collect do |word|
+    
   end
 end 
